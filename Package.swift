13c21d70d6f89265bcb3dbe328fe141573474bf8
RockfordWei <rocky@perfect.org>
2017-12-18 15:14:18 -0500

    Revert "Fixing node.js compatibilities"
    This reverts commit 8c7668946c7b14caff6f2442e686110d3e2b0f13.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,15 @@
 import PackageDescription
 import Foundation
 
+struct PackageSource: Codable {
+  public var url = ""
+  public var cache = ""
+  public var majorVersion = 0
+}
+
 struct LoadablePackage: Codable {
   public var name = ""
-  public var dependencies: [String: String] = [:]
-	public var dependencyVersions: [String: Int] = [:]
+  public var dependencies: [String: PackageSource] = [:]
 }
 
 let data = try? Data(contentsOf: URL(fileURLWithPath: "package.json"))
@@ -12,7 +17,14 @@ let pack = try JSONDecoder().decode(LoadablePackage.self, from: data!)
 
 let package = Package(
     name: pack.name, targets: [],
-    dependencies: pack.dependencies.map { (lib, url) in
-      return .Package(url: url, majorVersion: pack.dependencyVersions[lib] ?? 0 ) 
+    dependencies: pack.dependencies.map { (lib, source) in
+      let path: String
+      if let dir = opendir(source.cache) {
+        path = source.cache
+        closedir(dir)
+      } else {
+        path = source.url
+      }
+      return .Package(url: path, majorVersion: source.majorVersion ) 
     }
 )
