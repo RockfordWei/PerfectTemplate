f569c2f3af4559a44fbf6695b71196ee35d863de
RockfordWei <rocky@perfect.org>
2017-12-18 14:57:33 -0500

    Experimental `Codable` Package.

--- Package.swift
+++ Package.swift
@@ -1,19 +1,30 @@
 import PackageDescription
-#if os(Linux)
-import Glibc
-#else
-import Darwin
-#endif
+import Foundation
 
-var url = "https://github.com/PerfectlySoft/Perfect-HTTPServer.git"
-if let urlenv = getenv("URL_PERFECT_HTTPSERVER") {
-	url = String(cString: urlenv)
+struct PackageSource: Codable {
+  public var url = ""
+  public var cache = ""
+  public var majorVersion = 0
 }
 
+struct LoadablePackage: Codable {
+  public var name = ""
+  public var dependencies: [PackageSource] = []
+}
+
+let data = try? Data(contentsOf: URL(fileURLWithPath: "package.json"))
+let pack = try JSONDecoder().decode(LoadablePackage.self, from: data!)
+
 let package = Package(
-	name: "PerfectTemplate",
-	targets: [],
-	dependencies: [
-		.Package(url: url, majorVersion: 3),
-	]
+    name: pack.name, targets: [],
+    dependencies: pack.dependencies.map { source in
+      let path: String
+      if let dir = opendir(source.cache) {
+        path = source.cache
+        closedir(dir)
+      } else {
+        path = source.url
+      }
+      return .Package(url: path, majorVersion: source.majorVersion ) 
+    }
 )
