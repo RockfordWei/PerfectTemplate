d3568a2042226424c94ac7b21ac35209b0572aa3
RockfordWei <rocky@perfect.org>
2017-12-18 15:04:35 -0500

    Fixing compatibility of node.js.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ struct PackageSource: Codable {
 
 struct LoadablePackage: Codable {
   public var name = ""
-  public var dependencies: [PackageSource] = []
+  public var dependencies: [String: PackageSource] = [:]
 }
 
 let data = try? Data(contentsOf: URL(fileURLWithPath: "package.json"))
@@ -17,7 +17,7 @@ let pack = try JSONDecoder().decode(LoadablePackage.self, from: data!)
 
 let package = Package(
     name: pack.name, targets: [],
-    dependencies: pack.dependencies.map { source in
+    dependencies: pack.dependencies.map { (lib, source) in
       let path: String
       if let dir = opendir(source.cache) {
         path = source.cache
